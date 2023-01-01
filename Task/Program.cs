73f932b15c987528f7ae9848c870fbe8862fb990
Anastasia <3845303@gmail.com>
2023-01-01 20:00:00 +0900

    Task. An array of strings with a length less than or equal to three

---
+++ Task/Program.cs
@@ -0,0 +1,33 @@
+﻿int SearchNumberStrings(string[] arrayStrings)
+{
+    int count = 0;
+    for (int i = 0; i < arrayStrings.Length; i++)
+    {
+        if (arrayStrings[i].Length <= 3)
+            count++;
+    }
+    return count;
+}
+
+void PrintArray(string[] str)
+{
+    for (int i = 0; i < str.Length; i++) Console.Write($"{str[i]} ");
+    Console.WriteLine();
+}
+
+
+string[] strings = {"hello", "Array", "b123","01","mam"};
+int number=SearchNumberStrings(strings);
+string[] stringLength3 = new string[number];
+int j = 0;
+for (int i = 0; i < strings.Length; i++)
+{
+    if (strings[i].Length <= 3)
+    {
+        stringLength3[j] = strings[i];
+        j++;
+    }
+}
+
+PrintArray(strings);
+PrintArray(stringLength3);
